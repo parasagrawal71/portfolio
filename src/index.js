e391ddf0d2ce6362819aa99674089c040ed63574
Paras Agrawal <paras@piktorlabs.com>
2020-08-10 21:53:29 +0530

    Remove unneeded file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
 
 import App from "./pages/app/App";
 import "index.css";
-import reducers from "./redux/reducers";
 
-const store = createStore(reducers, applyMiddleware(thunk));
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/redux/actions/types.js
+++
@@ -1,5 +0,0 @@
-// TOAST MESSAGE
-export const TOAST_NEW_STATE = "TOAST_NEW_STATE";
-
-// CART and SAVE FOR LATER
-export const dummy = "eslint";
