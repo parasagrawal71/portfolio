6a7a67a055138e11628f6d8742c83a93799a93f1
Paras Agrawal <paras@piktorlabs.com>
2020-08-15 23:09:53 +0530

    feat: Add hamburger icon for mobile view

--- src/assets/Images.js
+++ src/assets/Images.js
@@ -1,7 +1,9 @@
 // IMPORTS //
 import _pLogo from "./img/p-logo.png";
 import _scrollToTop from "./img/scroll-to-top.png";
+import _hamBurger from "./img/hamburger.png";
 
 // EXPORTS //
 export const pLogo = _pLogo;
 export const scrollToTop = _scrollToTop;
+export const hamBurger = _hamBurger;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,11 @@
 import React from "react";
 
+// IMPORT USER-DEFINED COMPONENTS HERE //
+// import TextButton from "subComponents/textButton/TextButton";
+
 // IMPORT OTHERS HERE //
 import "./Header.scss";
-import { pLogo } from "assets/Images";
+import { pLogo, hamBurger } from "assets/Images";
 
 const Header = () => {
   return (
@@ -11,13 +14,17 @@ const Header = () => {
         <img src={pLogo} alt="p-logo" />
       </section>
 
-      <section className="header--right">
+      <section className="header--right-web">
         <a href="#">Home</a>
         <a href="#about">About</a>
         <a href="#skillset">Skillset</a>
         {/* <a href="#experience">Experience</a>
         <a href="#projects">Projects</a> */}
         <a href="#contact">Contact</a>
+      </section>
+
+      <section className="header--right-mobile">
+        <img src={hamBurger} alt="hamburger" />
       </section>
     </main>
   );
