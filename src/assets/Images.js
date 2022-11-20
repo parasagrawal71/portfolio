e4eab47d1822a152390ec4badd85e36b8222cff3
Paras Agrawal <pagrawal@nference.net>
2022-11-20 09:53:59 +0530

    Modify Images.js

--- src/assets/Images.js
+++ src/assets/Images.js
@@ -1,69 +1,74 @@
 // IMPORTS //
-import _pLogo from "./img/p-logo.png";
-import _scrollToTop from "./img/scroll-to-top.png";
-import _hamBurger from "./img/hamburger.png";
-import _lines from "./img/lines.svg";
-import _myPicture from "./img/paras_formal.jpg";
-import _frontend from "./img/frontend.svg";
-import _backend from "./img/backend.svg";
-import _halfAndroid from "./img/half-android.png";
-import _halfiOS from "./img/half-iOS.png";
-import _github from "./img/github.svg";
-import _linkedin from "./img/linkedin.svg";
-import _mail from "./img/mail.svg";
-import _instagram from "./img/instagram.svg";
-import _html5 from "./img/html5.png";
-import _css3 from "./img/css3.png";
-import _javascript from "./img/javascript.svg";
-import _react from "./img/react.png";
-import _reactNative from "./img/react-native.png";
-import _redux from "./img/redux.svg";
-import _vuejs from "./img/vuejs.png";
-import _nodejs from "./img/nodejs.png";
-import _python from "./img/python.png";
-import _mongodb from "./img/mongodb.png";
-import _git from "./img/git.png";
-import _linux from "./img/linux.png";
-import _aws from "./img/aws.png";
-import _api from "./img/api.png";
-import _apiDocumenterImage from "./img/api-documenter.png";
-import _shopAnywhereImage from "./img/shop-anywhere.png";
-import _twitterHelpdeskImage from "./img/twitter-helpdesk.png";
+import pLogo from "./img/p-logo.png";
+import scrollToTop from "./img/scroll-to-top.png";
+import hamBurger from "./img/hamburger.png";
+import lines from "./img/lines.svg";
+import myPicture from "./img/paras_formal.jpg";
+import frontend from "./img/frontend.svg";
+import backend from "./img/backend.svg";
+import halfAndroid from "./img/half-android.png";
+import halfiOS from "./img/half-iOS.png";
+import github from "./img/github.svg";
+import linkedin from "./img/linkedin.svg";
+import mail from "./img/mail.svg";
+import instagram from "./img/instagram.svg";
+import html5 from "./img/html5.png";
+import css3 from "./img/css3.png";
+import javascript from "./img/javascript.svg";
+import react from "./img/react.png";
+import reactNative from "./img/react-native.png";
+import redux from "./img/redux.svg";
+import vuejs from "./img/vuejs.png";
+import nodejs from "./img/nodejs.png";
+import python from "./img/python.png";
+import mongodb from "./img/mongodb.png";
+import git from "./img/git.png";
+import linux from "./img/linux.png";
+import aws from "./img/aws.png";
+import api from "./img/api.png";
+import apiDocumenterImage from "./img/api-documenter.png";
+import shopAnywhereImage from "./img/shop-anywhere.png";
+import twitterHelpdeskImage from "./img/twitter-helpdesk.png";
+
+const kafka =
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/apachekafka/apachekafka-original.svg";
+const sass = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg";
 
 // EXPORTS //
-export const pLogo = _pLogo;
-export const scrollToTop = _scrollToTop;
-export const hamBurger = _hamBurger;
-export const lines = _lines;
-export const myPicture = _myPicture;
-export const frontend = _frontend;
-export const backend = _backend;
-export const halfAndroid = _halfAndroid;
-export const halfiOS = _halfiOS;
-export const github = _github;
-export const linkedin = _linkedin;
-export const mail = _mail;
-export const instagram = _instagram;
-export const html5 = _html5;
-export const css3 = _css3;
-export const javascript = _javascript;
-export const react = _react;
-export const reactNative = _reactNative;
-export const redux = _redux;
-export const vuejs = _vuejs;
-export const nodejs = _nodejs;
-export const python = _python;
-export const mongodb = _mongodb;
-export const git = _git;
-export const linux = _linux;
-export const aws = _aws;
-export const api = _api;
-export const kafka =
-  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/apachekafka/apachekafka-original.svg";
-export const sass = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg";
-export const apiDocumenterImage = _apiDocumenterImage;
-export const shopAnywhereImage = _shopAnywhereImage;
-export const twitterHelpdeskImage = _twitterHelpdeskImage;
+export {
+  pLogo,
+  scrollToTop,
+  hamBurger,
+  lines,
+  myPicture,
+  frontend,
+  backend,
+  halfAndroid,
+  halfiOS,
+  github,
+  linkedin,
+  mail,
+  instagram,
+  html5,
+  css3,
+  javascript,
+  react,
+  reactNative,
+  redux,
+  vuejs,
+  nodejs,
+  python,
+  mongodb,
+  git,
+  linux,
+  aws,
+  api,
+  apiDocumenterImage,
+  shopAnywhereImage,
+  twitterHelpdeskImage,
+  kafka,
+  sass,
+};
 
 /**
  * Get programmiong languages icons from this site "https://devicon.dev/"
