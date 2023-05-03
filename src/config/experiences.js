32baba05f7e57efeddfb197021333a8ffb2d9708
Paras Agrawal <parasagrawal71@gmail.com>
2023-05-03 09:27:34 +0530

    Rephrase work experience bullet points

--- src/config/experiences.js
+++ src/config/experiences.js
@@ -44,23 +44,23 @@ export const experienceList = [
     companyIntro: "",
     works: [
       {
-        bulletPoint: `Built RESTful APIs to carefully design, test, and build new features that resolved issues and advanced software.`,
+        bulletPoint: `Driven small projects to re-architect the system's components improving its robustness and resilience.`,
         category: BACKEND,
       },
       {
-        bulletPoint: `Implemented a Node.js-based load balancer to efficiently manage the processes to run on multiple machines in parallel in order to convert 25,000 DZI images into DICOM format.`,
+        bulletPoint: `Redesigned the architecture of the node server to improve code maintainability and reduce issues, ultimately minimizing the resources required for development and testing.`,
         category: BACKEND,
       },
       {
-        bulletPoint: `Oversaw software deployment and took the initiative to develop scripts that increased the level of automation and efficiency of deploying and configuring micro-services.`,
+        bulletPoint: `Upgraded inter-service communication by implementing a RabbitMQ-based message queue to enhance the system's availability and fault tolerance, which resulted in a 50% reduction in errors.`,
+        category: BACKEND,
+      },
+      {
+        bulletPoint: `Collaborated with the DevOps team to deploy applications to the cloud, utilizing HAProxy and Nginx servers.`,
         category: DEVOPS,
       },
       {
-        bulletPoint: `Collaborated with the DevOps team to deploy applications to the cloud using HAProxy and Nginx servers.`,
-        category: DEVOPS,
-      },
-      {
-        bulletPoint: `Perform code and design reviews to ensure code quality.`,
+        bulletPoint: `Performed code reviews, implemented best practices, and enforced coding standards to ensure high-quality code and maintainable systems.`,
         category: OTHERS,
       },
     ],
@@ -75,23 +75,31 @@ export const experienceList = [
     companyIntro: "",
     works: [
       {
-        bulletPoint: `Set up communication across multiple applications using Kafka to reflect data in real-time.`,
+        bulletPoint: `Played a significant role in helping the company secure a 10 million dollar long-term contract by developing robust APIs and user-friendly UI screens.`,
         category: BACKEND,
       },
       {
-        bulletPoint: `Implemented a seamless deployment method of the applications in multiple modes and environments.`,
+        bulletPoint: `Integrated Kafka to facilitate real-time data synchronization across multiple applications.`,
+        category: BACKEND,
+      },
+      {
+        bulletPoint: `Achieved a 75% reduction in deployment time by implementing a seamless deployment method of the applications in multiple modes and environments.`,
         category: DEVOPS,
       },
       {
-        bulletPoint: `Developed cross-platform compatible desktop application to view DZI (Deep Zoom Image) using electron builder.`,
-        category: FRONTEND,
-      },
-      {
-        bulletPoint: `Improved test coverage by setting up an end-to-end and unit test framework to reduce the risk of breaking production and to perform regression testing.`,
+        bulletPoint: `Developed an efficient Node.js-based load balancer to manage parallel processing across multiple machines, resulting in the successful conversion of 25K DZI images into DICOM format.`,
         category: BACKEND,
       },
       {
-        bulletPoint: `Employ best coding practices to create user interfaces to improve the user experience and performance.`,
+        bulletPoint: `Received the "Ngenious Award" in recognition of my outstanding performance.`,
+        category: OTHERS,
+      },
+      {
+        bulletPoint: `Employed best coding practices to thoughtfully design, test, and implement new features that effectively address issues and drive software forward.`,
+        category: FRONTEND,
+      },
+      {
+        bulletPoint: `Developed cross-platform compatible desktop application to view DZI (Deep Zoom Image) using electron builder.`,
         category: FRONTEND,
       },
     ],
@@ -106,15 +114,15 @@ export const experienceList = [
     companyIntro: "",
     works: [
       {
-        bulletPoint: `Built Serverless API server from scratch using Nodejs and Mongoose hosted on AWS. Also, worked on Nodejs and Express API Server hosted on AWS elastic beanstalk.`,
+        bulletPoint: `Transformed Express API Server hosted on AWS elastic beanstalk into Serverless APIs using Node.js and AWS Lambda entirely from scratch, which led to a decrease in server downtime.`,
         category: BACKEND,
       },
       {
-        bulletPoint: `Played a major role in the integration of the ADP APIs and launch GoTag, an employee portal application, on the ADP Marketplace.`,
+        bulletPoint: `Played a key role in the integration of the ADP APIs and launch GoTag, an employee portal application, on the ADP Marketplace.`,
         category: BACKEND,
       },
       {
-        bulletPoint: `Worked on a few projects at this company throughout my time there, and I was able to apply a variety of technologies. Some of the technologies used include React.js, Redux, Node.js MongoDB, Vue.js, Python, React Native, and AWS.`,
+        bulletPoint: `Throughout my tenure at this company, I worked on several projects and gained experience in utilizing a diverse range of technologies such as React.js, Redux, Node.js, MongoDB, Vue.js, Python, React Native, and AWS.`,
         category: FRONTEND,
       },
     ],
@@ -127,6 +135,11 @@ export const experienceList = [
     companyType: "Product-based",
     companyWebsite: "",
     companyIntro: "",
-    works: [],
+    works: [
+      {
+        bulletPoint: `Worked as a full-stack developer at this company on various websites, acquiring proficiency in diverse technologies such as JavaScript, HTML, CSS, Bootstrap, and PHP.`,
+        category: FRONTEND,
+      },
+    ],
   },
 ];
